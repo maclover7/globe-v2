197d61bbcc76f7224cce0c117e68149979499608
Jon Moss <me@jonathanmoss.me>
2015-11-05 18:56:57 -0500

    Generate new Pliny application

---
+++ README.md
@@ -0,0 +1,3 @@
+# Globe V2
+
+A new and improved version of Globe, created using [Pliny](https://github.com/interagent/pliny) and [Ember.js](emberjs.com).
---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+# This file should contain all the record creation needed to seed the database
+# with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the
+# db with db:setup).
+#
+# Seeding can occur multiple times during the execution of a single Rake task
+# because seeding should occur in all environments (development, testing,
+# etc.). The currently connected database can be accessed via
+# `Sequel::DATABASES.last`.
